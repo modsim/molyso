4d9fa9d57cc05c28bcb2b42be69d126bea42b262
Christian Sachs <c.sachs@fz-juelich.de>
2017-05-18 13:17:14 +0200

    fixed the test runner, it was not reporting the test outcome via status

--- molyso/test/__main__.py
+++ molyso/test/__main__.py
@@ -3,6 +3,9 @@
 The test module's __main__ contains the main() function to run the doctests.
 """
 
+import sys
+import doctest
+
 
 def main():
     """
@@ -35,36 +38,50 @@ def main():
     import molyso.mm.tracking_infrastructure
     import molyso.mm.tracking_output
 
-    import doctest
+    modules_to_test = [
+        molyso.generic.rotation,
+        #
+        molyso.generic.etc,
+        molyso.generic.fft,
+        #
+        molyso.generic.registration,
+        molyso.generic.rotation,
+        molyso.generic.signal,
+        molyso.generic.smoothing,
+        molyso.generic.tunable,
+        #
+        molyso.imageio.imagestack,
+        molyso.imageio.imagestack_ometiff,
+        molyso.imageio.imagestack_czi,
+        molyso.imageio.imagestack_nd2,
+        #
+        molyso.debugging.debugplot,
+        #
+        molyso.mm.cell_detection,
+        molyso.mm.channel_detection,
+        molyso.mm.fluorescence,
+        molyso.mm.highlevel,
+        molyso.mm.highlevel_interactive_ground_truth,
+        molyso.mm.highlevel_interactive_viewer,
+        molyso.mm.image,
+        molyso.mm.tracking,
+        molyso.mm.tracking_infrastructure,
+        molyso.mm.tracking_output
+    ]
 
-    doctest.testmod(molyso.generic.rotation)
+    total_failures, total_tests = 0, 0
 
-    doctest.testmod(molyso.generic.etc)
-    doctest.testmod(molyso.generic.fft)
+    for a_module in modules_to_test:
+        failures, tests = doctest.testmod(a_module)
+        total_failures += failures
+        total_tests += tests
 
-    doctest.testmod(molyso.generic.registration)
-    doctest.testmod(molyso.generic.rotation)
-    doctest.testmod(molyso.generic.signal)
-    doctest.testmod(molyso.generic.smoothing)
-    doctest.testmod(molyso.generic.tunable)
+    print("Run %d tests in total." % (total_tests,))
 
-    doctest.testmod(molyso.imageio.imagestack)
-    doctest.testmod(molyso.imageio.imagestack_ometiff)
-    doctest.testmod(molyso.imageio.imagestack_czi)
-    doctest.testmod(molyso.imageio.imagestack_nd2)
+    if total_failures > 0:
+        print("Test failures occurred, exiting with non-zero status.")
+        sys.exit(1)
 
-    doctest.testmod(molyso.debugging.debugplot)
-
-    doctest.testmod(molyso.mm.cell_detection)
-    doctest.testmod(molyso.mm.channel_detection)
-    doctest.testmod(molyso.mm.fluorescence)
-    doctest.testmod(molyso.mm.highlevel)
-    doctest.testmod(molyso.mm.highlevel_interactive_ground_truth)
-    doctest.testmod(molyso.mm.highlevel_interactive_viewer)
-    doctest.testmod(molyso.mm.image)
-    doctest.testmod(molyso.mm.tracking)
-    doctest.testmod(molyso.mm.tracking_infrastructure)
-    doctest.testmod(molyso.mm.tracking_output)
 
 if __name__ == '__main__':
     main()
